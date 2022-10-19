1d2a8c59785d496e14b272938024922a61d57a4f
Li Runze <lirunze.me@gmail.com>
2022-10-19 16:22:31 +0800

    1000

---
+++ 1000.cpp
@@ -0,0 +1,16 @@
+/*
+A+B Problem
+Input: Two integer a,b (0<=a,b<=10)
+Output: a+b
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+
+    return 0;
+}
