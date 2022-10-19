a393fb642cf2c49ffa6a8de6e4bceabe055616a3
Li Runze <lirunze.me@gmail.com>
2022-10-19 20:11:56 +0800

    1003

--- 1004.cpp
+++ 1004.cpp
@@ -1,3 +1,9 @@
+/*
+Financial Management
+Input: The input will be twelve lines. Each line will contain the closing balance of his bank account for a particular month. Each number will be positive and displayed to the penny. No dollar sign will be included.
+Output: The output will be a single number, the average (mean) of the closing balances for the twelve months. It will be rounded to the nearest penny, preceded immediately by a dollar sign, and followed by the end-of-line. There will be no other spaces or characters in the output.
+*/
+
 #include<iostream>
 #include<iomanip>
 using namespace std;
